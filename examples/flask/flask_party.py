dc5e0baebc6af7644340a610afe307ee2ef58cd2
Ron DuPlain <ron.duplain@gmail.com>
2012-03-15 02:16:36 -0400

    Update Flask example for new APIs.

--- examples/flask/flask_party.py
+++ examples/flask/flask_party.py
@@ -1,16 +1,24 @@
-from flask import Flask, request
-from wsgi_party import WSGIParty, PartylineConnector
+from flask import Flask, abort, request
+from wsgi_party import WSGIParty
 
 
-class PartylineFlask(Flask, PartylineConnector):
+class PartylineFlask(Flask):
     def __init__(self, import_name, *args, **kwargs):
         super(PartylineFlask, self).__init__(import_name, *args, **kwargs)
         self.add_url_rule(WSGIParty.invite_path, endpoint='partyline',
-                          view_func=self.join_party_wrapper)
+                          view_func=self.join_party)
+        self.partyline = None
+        self.connected = False
 
-    def join_party_wrapper(self, request=request):
-        """A simple wrapper to support Flask's request pattern."""
-        return self.join_party(request.environ)
+    def join_party(self, request=request):
+        # Bootstrap, turn the view function into a 404 after registering.
+        if self.connected:
+            # This route does not exist at the HTTP level.
+            abort(404)
+        self.partyline = request.environ.get(WSGIParty.partyline_key)
+        self.partyline.connect('ping', lambda x: 'pong')
+        self.connected = True
+        return 'ok'
 
 
 # Demonstrate.
@@ -24,6 +32,15 @@ two.debug = True
 
 one.config['APPLICATION_ROOT'] = '/one'
 two.config['APPLICATION_ROOT'] = '/two'
+
+@root.route('/', endpoint='index')
+def root_index():
+    if root.partyline is None:
+        return 'I have no friends.'
+    # Note: This is a synchronous call.
+    pongs = root.partyline.send_all('ping', None)
+    # Simply show responses.
+    return repr(list(pongs))
 
 application = WSGIParty(root, {
     '/one': one,
